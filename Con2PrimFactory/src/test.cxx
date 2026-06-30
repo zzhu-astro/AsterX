0797b310b95ba719d05a56211330f0d2c3dd86ef
Zhenyu Zhu <zhenyu.zzhu@outlook.com>
2026-06-30 15:09:12 -0500

    minor

--- Con2PrimFactory/src/test.cxx
+++ Con2PrimFactory/src/test.cxx
@@ -37,13 +37,13 @@ extern "C" void Con2PrimFactory_Test(CCTK_ARGUMENTS) {
       const CCTK_REAL temp_test = 0.2;
       const CCTK_REAL Ye_test = 0.5;
       const CCTK_REAL press_tau0 = eos_3p_rad_ig->press_from_rho_temp_ye_tau(
-          rho_test, temp_test, Ye_test, 0.0);
+          rho_test, temp_test, Ye_test, EOSX::optical_depths{0.0, 0.0, 0.0});
       const CCTK_REAL eps_tau0 = eos_3p_rad_ig->eps_from_rho_temp_ye_tau(
-          rho_test, temp_test, Ye_test, 0.0);
+          rho_test, temp_test, Ye_test, EOSX::optical_depths{0.0, 0.0, 0.0});
       const CCTK_REAL press_tau1 = eos_3p_rad_ig->press_from_rho_temp_ye_tau(
-          rho_test, temp_test, Ye_test, 1.0);
+          rho_test, temp_test, Ye_test, EOSX::optical_depths{1.0, 1.0, 1.0});
       const CCTK_REAL cs_tau1 = eos_3p_rad_ig->csnd_from_rho_temp_ye_tau(
-          rho_test, temp_test, Ye_test, 1.0);
+          rho_test, temp_test, Ye_test, EOSX::optical_depths{1.0, 1.0, 1.0});
 
       assert(fabs(press_tau0 - rho_test * temp_test) <=
              1.0e-12 * fmax(1.0, fabs(rho_test * temp_test)));
